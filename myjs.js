cf51ad9cbdea9588e22386dfd3ef7f0936bf852c
bbaccarella <brianbaccarella@bloominbrands.com>
2020-04-16 16:26:13 -0400

    comment out carousel pause func

--- myjs.js
+++ myjs.js
@@ -3,33 +3,19 @@ $('#myModal').on('shown.bs.modal', function () {
 });
 
 
-var carouselState = false;
+// var carouselState = false;
 
-$('#home-pause').click(function () {
+// $('#home-pause').click(function () {
 
  
-  if(carouselState){
-    $('#homeCarousel').carousel('cycle');
-  } else {
-    $('#homeCarousel').carousel('pause');
-  }
-  
-  $('#home-pause').toggleClass('play-btn');
-  carouselState = !carouselState;
-});
-
-
-
-// $("home-pause").click(function() {
-//   if ($(this).attr("id") === "home-pause") {
-//     $('#homeCarousel').carousel('pause');
-//     $(this).attr("id", "home-play");
-//     $("button", this).toggleClass("glyphicon-play glyphicon-pause");
-//   } else {
+//   if(carouselState){
 //     $('#homeCarousel').carousel('cycle');
-//     $(this).attr("id", "home-pause");
-//     $("button", this).toggleClass("glyphicon-pause glyphicon-play");
+//   } else {
+//     $('#homeCarousel').carousel('pause');
 //   }
+  
+//   $('#home-pause').toggleClass('play-btn');
+//   carouselState = !carouselState;
 // });
 
 //Picture element HTML5 shiv

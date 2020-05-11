e137390aa516ff3bf75f8f8e8bc1ed9fb09484e0
bbaccarella <brianbaccarella@bloominbrands.com>
2020-05-11 15:12:28 -0400

    seperate js and jQuery files

---
+++ simpleJavaScript.js
@@ -0,0 +1,8 @@
+// There is a bug in safari dealing with the navbar's border image. Check if Safari = true & chrome = false, then remove the class with the border image.
+if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
+{
+  $('.sticky-top').removeClass('nav-border');      
+}
+
+//Fallback for browsers that don't support the HTML5 Picture element
+document.createElement("picture");

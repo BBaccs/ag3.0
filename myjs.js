3f538ddcbce3a9aa87dd8e2ccd79ff2e9a10f940
Brian <baccarellab@gmail.com>
2020-05-24 23:24:33 -0400

    add doctype remove safari buf fix

--- myjs.js
+++ myjs.js
@@ -6,22 +6,26 @@ $('#myModal').on('shown.bs.modal', function () {
 
 var carouselState = false;
 
-$('.pause-btn').click(function () {
+
+$('.pause-button').click(function () {
+  
   if(carouselState){
     $('.carousel').carousel('cycle');
+    console.log('cycle')
   } else {
     $('.carousel').carousel('pause');
+    console.log('pause')
   }
-  $('#home-pause').toggleClass('play-btn');
+  // $('#home-pause').toggleClass('play-btn');
   carouselState = !carouselState;
 });
 
 
 // There is a bug in safari dealing with the navbar's border image. Check if Safari = true & chrome = false, then remove the class with the border image.
-if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
-{
-  $('.nav-border').removeClass('nav-border');      
-}
+// if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
+// {
+//   $('.nav-border').removeClass('nav-border');      
+// }
 
 //Fallback for browsers that don't support the HTML5 Picture element
 document.createElement("picture");

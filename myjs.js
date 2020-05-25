1b3c7ed0dd56ea859e2ca15a050e160c886efcf2
Brian <baccarellab@gmail.com>
2020-05-25 11:50:47 -0400

    remove duplicate js, add script

--- myjs.js
+++ myjs.js
@@ -3,9 +3,7 @@ $('#myModal').on('shown.bs.modal', function () {
 });
 
 // Pause / play btn funtionality
-
 var carouselState = false;
-
 
 $('.pause-button').click(function () {
   
@@ -19,13 +17,3 @@ $('.pause-button').click(function () {
   // $('#home-pause').toggleClass('play-btn');
   carouselState = !carouselState;
 });
-
-
-// There is a bug in safari dealing with the navbar's border image. Check if Safari = true & chrome = false, then remove the class with the border image.
-if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
-{
-  $('.nav-border').removeClass('nav-border');      
-}
-
-//Fallback for browsers that don't support the HTML5 Picture element
-document.createElement("picture");

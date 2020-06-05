eb8e29217b233bfa1f6be422db202464d9991ae7
bbaccarella <brianbaccarella@bloominbrands.com>
2020-06-05 14:31:17 -0400

    delete simplejs

--- simpleJavaScript.js
+++
@@ -1,8 +0,0 @@
-// There is a bug in safari dealing with the navbar's border image. Check if Safari = true & chrome = false, then remove the class with the border image.
-// if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
-// {
-//   $('.nav-border').removeClass('nav-border');      
-// }
-
-// Fallback for browsers that don't support the HTML5 Picture element
-document.createElement("picture");

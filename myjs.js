0078e52f7c95c49c0e6249e3faaabdd7a0918549
Brian <baccarellab@gmail.com>
2020-07-03 13:35:17 -0400

    remove comment

--- myjs.js
+++ myjs.js
@@ -17,11 +17,3 @@ $('.pause-button').click(function () {
 
 // Fallback for browsers that don't support the HTML5 Picture element
 document.createElement("picture");
-
-
-// This bugfix was removed because the nav border was removed but it will remain here in case we decide to bring it back.
-// There is a bug in safari dealing with the navbar's border image. Check if Safari = true & chrome = false, then remove the class with the border image.
-// if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
-// {
-//   $('.nav-border').removeClass('nav-border');      
-// }

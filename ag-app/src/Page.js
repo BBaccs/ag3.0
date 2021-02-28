5898e0841017a33f7e5dfba4e771598f6aaabd3e
Brian <baccarellab@gmail.com>
2021-02-28 11:40:13 -0500

    wrapper page comp

---
+++ ag-app/src/Page.js
@@ -0,0 +1,14 @@
+// If you want to use a wrapper component
+
+import React, { Component } from 'react';
+import "./styles/Page.css";
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="page">{children}</div>;
+    )
+  }
+}
+
+export default page;
